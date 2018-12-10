c80d55f220f8829d23d5ba5583beffd9daee00d0
Daniel Fries <daniel.w.fries@gmail.com>
2018-12-10 05:29:42 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,17 @@
 class Hash
   def keys_of(arguments)
-    # code goes here
+    ## identify the Hash in question with "self"
+    
+    ##  takes an undefined number of arguments 
+    ##  using the splat operator 
+    ##  and returns an array 
+    ##  with every key from the hash 
+    ##  whose value matches the value(s) given as an argument
+
+    ##  you give it values, it gives you the corresponding keys
+    keys = []
+    self.each do |key, value|
+      
+    
   end
 end
